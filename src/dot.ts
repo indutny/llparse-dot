a4e4dc0cf0efe88c84f4baf1c43cbd49daef68fa
Fedor Indutny <fedor@indutny.com>
2018-03-19 17:00:03 -0400

    dot: merge otherwise

--- src/dot.ts
+++ src/dot.ts
@@ -67,11 +67,16 @@ export class Dot {
 
   private buildNode(node: Node): string {
     let res = '';
-    const name = this.escape(this.id(node));
+    const edges = Array.from(node);
+
+    const otherwise = node.getOtherwiseEdge();
+    if (otherwise !== undefined) {
+      edges.push(otherwise);
+    }
 
     const advance: Map<Node, Edge[]> = new Map();
     const noAdvance: Map<Node, Edge[]> = new Map();
-    for (const edge of node) {
+    for (const edge of edges) {
       const targets = edge.noAdvance ? noAdvance : advance;
       if (targets.has(edge.node)) {
         targets.get(edge.node)!.push(edge);
@@ -83,14 +88,6 @@ export class Dot {
     res += this.buildEdgeMap(node, advance, 'advance');
     res += this.buildEdgeMap(node, noAdvance, 'noAdvance');
 
-    const otherwise = node.getOtherwiseEdge();
-    if (otherwise !== undefined) {
-      const label = this.buildOtherwiseLabel(node, otherwise);
-      const color = otherwise.noAdvance ? COLOR_NO_ADVANCE : COLOR_ADVANCE;
-      res += `  "${this.id(node)}" -> "${this.id(otherwise.node)}" ` +
-        `[label="${label}" color="${color}"];\n`;
-    }
-
     return res;
   }
 
@@ -98,12 +95,13 @@ export class Dot {
     let res = '';
 
     map.forEach((edges, target) => {
+      const otherwise: Edge[] = [];
       const single: Edge[] = [];
       const sequence: Edge[] = [];
       const code: Edge[] = [];
       for (const edge of edges) {
         if (edge.key === undefined) {
-          assert(false, 'Unexpected otherwise edge');
+          otherwise.push(edge);
         } else if (typeof edge.key === 'number') {
           code.push(edge);
         } else if (edge.key.length === 1) {
@@ -153,6 +151,10 @@ export class Dot {
 
       for (const edge of code) {
         labels.push(this.buildInvokeLabel(node, edge));
+      }
+
+      for (const edge of otherwise) {
+        labels.push(this.buildOtherwiseLabel(node, edge));
       }
 
       const color = kind === 'noAdvance' ? COLOR_NO_ADVANCE : COLOR_ADVANCE;
